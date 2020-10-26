bf146debefa505b46323db1556c54ac58f9ab900
p-t-a-p-1 <kb240225+1@gmail.com>
2020-10-26 23:14:46 +0900

    [add] トップのコンポーネントファイル追加

---
+++ components/pages/topComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Upload />
+  </div>
+</template>
+
+<script>
+import Upload from '@/components/templates/upload.vue'
+export default {
+  components: {
+    Upload,
+  },
+}
+</script>
