e615ccc80a0fceb026c82f18925dabc42015731f
p-t-a-p-1 <kb240225+1@gmail.com>
2020-10-27 22:29:28 +0900

    [add] 画像投稿フォームコンポーネントファイル追加

--- components/templates/upload.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>main</h1>
-  </div>
-</template>
---
+++ components/templates/uploadForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="uploadForm">
+    <h1 class="upload_title">Upload your image</h1>
+    <p class="upload_note">File should be Jpeg, Png,...</p>
+    <div class="upload_main">
+      <figure class="upload_main_drop">
+        <img src="@/assets/img/image.svg" width="" height="" />
+        <p class="-text">Drag & Drop your image here</p>
+      </figure>
+      <p class="upload_main_text">Or</p>
+      <div class="upload_main_button">
+        <label class="-label">
+          Choose a file
+          <input type="file" />
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.upload {
+  &_title {
+    font-size: 1.8rem;
+  }
+}
+</style>
